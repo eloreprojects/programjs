6dd58be7ca1cf5cfbf60d36872787b6400272607
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-07-18 22:41:20 -0700

    Add mocha and chai, and finish test structure

--- test/bubblesort_test.js
+++ test/bubblesort_test.js
@@ -2,11 +2,18 @@ const bubble_sort = require('../src/algorithms/bubble_sort');
 
 // Test Bubble Sort
 // To do: generate array of random numbers and then test incremental values with loop
+class BubbleSortTest {
 
-module.exports = () => {
-    let array = [6, 4, 7, 3, 2];
+    // Temporary bubblesort test
+    static overall() {
+        let array = [6, 4, 7, 3, 2];
 
-    console.log("Initial Array: ", array);
-    const sorted_array = bubble_sort(array);
-    console.log("Sorted Array: ", sorted_array);
+        console.log("Initial Array: ", array);
+        const sorted_array = bubble_sort(array);
+        console.log("Sorted Array: ", sorted_array);
+
+        return true;
+    }
 }
+
+module.exports = BubbleSortTest;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
+'use strict';
+
+var expect = require('chai').expect;
+
 const stack_test = require('./stack_test')
 const bubblesort_test = require('./bubblesort_test');
 
-// Data Structures
-stack_test();
+describe('#stack_test', function() {
+    it('should pass test in stack_test', function() {
+        expect(stack_test.overall()).to.equal(true);
+    });
+});
 
-// Algorithms
-bubblesort_test();
+describe('#bubblesort_test', function() {
+    it('should pass test in bubblesort_test', function() {
+        expect(bubblesort_test.overall()).to.equal(true);
+    });
+});

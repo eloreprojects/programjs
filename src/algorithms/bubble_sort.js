5ada3fd1cab12232d888ea43b7e28ecdc2dee8cf
arun <arun.kirubarajan@gmail.com>
2017-07-20 06:38:31 -0400

    Edit code conventions to match guidelines of bubble_sort

--- src/algorithms/bubble_sort.js
+++ src/algorithms/bubble_sort.js
@@ -1,14 +1,13 @@
-// BubbleSort Implementation
-// Worst case is O(n2)
+// Bubble Sort Implementation of O(n^2)
+
 module.exports = (array) => {
-  for(let i = 0; i < array.length; i++) {
-    for(let j = 1; j < array.length; j++) {
-        // if previous is larger than current
-        if(array[j - 1] > array[j]) {
-            // swap elements
-            [array[j - 1], array[j]] = [array[j], array[j - 1]];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+        if (array[j - 1] > array[j]) {
+          [array[j - 1], array[j]] = [array[j], array[j - 1]];
       }
     }
   }
+  
   return array;
 };

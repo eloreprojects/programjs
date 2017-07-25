e5a9ecec3b3c72630b0e0722397b5f73ba392cea
thearnavgarg <thearnavgarg@gmail.com>
2017-07-24 21:58:28 -0700

    Adding the TreeNode and Binary Tree implementation

---
+++ src/dataStructures/trees/treeNode.js
@@ -0,0 +1,67 @@
+class TreeNode {
+
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  getLeftNode() {
+    return this.left;
+  }
+
+  getRigthNode() {
+    return this.right;
+  }
+
+  setValue(value) {
+    this.value = value;
+  }
+
+  setLeftNode(left) {
+    this.left = left;
+  }
+
+  setRightNode(right) {
+    this.right = right;
+  }
+
+  static preorderTraversal(head) {
+    if (head === null) {
+      return;
+    }
+    console.log(head.value);
+    this.preorderTraversal(head.left);
+    this.preorderTraversal(head.right);
+  }
+
+  static inorderTraversal(head) {
+    if (head === null) {
+      return;
+    }
+    this.inorderTraversal(head.left);
+    console.log(head.value);
+    this.inorderTraversal(head.right);
+  }
+
+  static postorderTraversal(head) {
+    if (head === null) {
+      return;
+    }
+    this.postorderTraversal(head.left);
+    this.postorderTraversal(head.right);
+    console.log(head.value);
+  }
+
+  static displayTree(head) {
+    // TODO
+  }
+}
+
+module.exports = {
+  TreeNode
+}

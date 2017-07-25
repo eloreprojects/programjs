b2492fbefd4381e4fe4ad52bb3ee36935392284c
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-07-25 14:23:40 -0700

    Add DoublyLinkedList implementation

--- src/dataStructures.js
+++ src/dataStructures.js
@@ -1,11 +1,15 @@
 const node = require('./dataStructures/node');
 const doubleNode = require('./dataStructures/doubleNode');
+const singlyLinkedList = require('./dataStructures/singlyLinkedList');
+const doublyLinkedList = require('./dataStructures/doublyLinkedList');
 const stack = require('./dataStructures/stack');
 const queue = require('./dataStructures/queue');
 
 module.exports = {
   node,
   doubleNode,
+  singlyLinkedList,
+  doublyLinkedList,
   stack,
   queue,
 };

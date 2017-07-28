2ac9bd683418eba422960973d65a1f4e93f48305
arun <arun.kirubarajan@gmail.com>
2017-07-28 12:07:01 -0400

    Update factorial to recursive implementation

--- src/algorithms/factorial.js
+++ src/algorithms/factorial.js
@@ -1,25 +1,11 @@
-const calculateFactorial = (n) => {
-  if (n < 0) {
-    return undefined;
-  }
-  let result = 1;
-  if (n === 0 || n === 1) {
-    return result;
-  }
-  for (let i = 1; i <= n; i += 1) {
-    result *= i;
-  }
-  return result;
-};
-
-module.exports = (n) => {
-  if (Array.isArray(n) && n.length !== 0) {
-    n = n.map(calculateFactorial);
-  } else if (Number.isInteger(n)) {
-    n = calculateFactorial(n);
+const factorial = (n) => {
+  if (n >= 1) {
+    return n * factorial(n - 1); 
+  } else if (n < 0) {
+    return -1;
   } else {
-    n = 'Argument not a number.';
+    return 1;
   }
-  return n;
 };
 
+module.exports = factorial;

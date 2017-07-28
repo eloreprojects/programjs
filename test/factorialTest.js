b4b638f9ac4dff934cff751258685e96c9eb8fe7
arun <arun.kirubarajan@gmail.com>
2017-07-28 12:06:45 -0400

    Fixed factorial test

--- test/factorialTest.js
+++ test/factorialTest.js
@@ -2,14 +2,23 @@ const factorial = require('../src/algorithms/factorial');
 
 module.exports = () => {
   const numbers = [-1, 0, 1, 2, 5, 10];
-  const answers = [undefined, 1, 1, 2, 120, 3628800];
-  console.log(`Initial Array: ${numbers.join(' ')}`);
-  const factorialResult = factorial(numbers);
-  console.log(`Factorial Array: ${numbers.join(' ')}`);
-  for (let i = 0; i < factorialResult.length; i += 1) {
-    if (factorialResult[i] !== answers[i]) {
-      return false;
+  const answers = [-1, 1, 1, 2, 120, 3628800];
+  const attempt = [];
+  let flag = false;
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    attempt.push(factorial(numbers[i]));
+    if (attempt[i] !== answers[i]) {
+      flag = true;
     }
+  }
+
+  console.log(`Input array: ${numbers.join(' ')}`);
+  console.log(`Answer array: ${answers.join(' ')}`);
+  console.log(`Attempted array: ${attempt.join(' ')}`);
+
+  if (flag) {
+    return false;
   }
   return true;
 };

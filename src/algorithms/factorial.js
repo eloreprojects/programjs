7d7f122639b783a4bd197e6d902fd02876ec26c9
arun <arun.kirubarajan@gmail.com>
2017-07-28 12:10:16 -0400

    Fix lint errors

--- src/algorithms/factorial.js
+++ src/algorithms/factorial.js
@@ -1,11 +1,10 @@
 const factorial = (n) => {
   if (n >= 1) {
-    return n * factorial(n - 1); 
+    return n * factorial(n - 1);
   } else if (n < 0) {
     return -1;
-  } else {
-    return 1;
   }
+  return 1;
 };
 
 module.exports = factorial;

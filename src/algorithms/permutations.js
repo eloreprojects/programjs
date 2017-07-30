dc0f2145b2ddb2d1b50d72e9c66e23a11fbe1c98
arun <arun.kirubarajan@gmail.com>
2017-07-30 11:34:14 -0400

    Fix lint issues of indent sizes

--- src/algorithms/permutations.js
+++ src/algorithms/permutations.js
@@ -1,8 +1,8 @@
 const factorial = require('./factorial');
 
 module.exports = (n, r) => {
-    if (!(n >= r > 0)) {
-        return -1;
-    }
-    return (factorial(n) / factorial(n - r));
+  if (!(n >= r) && (r > 0)) {
+      return -1;
+  }
+  return (factorial(n) / factorial(n - r));
 };

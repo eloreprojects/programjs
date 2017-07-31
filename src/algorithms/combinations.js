82533bf808c0ee31340d0c4aa4ddfc16e79a3e6c
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2017-07-30 23:11:17 -0700

    Linting errors

--- src/algorithms/combinations.js
+++ src/algorithms/combinations.js
@@ -2,7 +2,7 @@ const factorial = require('./factorial');
 
 module.exports = (n, r) => {
   if (!(n >= r) && (r > 0)) {
-      return -1;
+    return -1;
   }
   return (factorial(n) / (factorial(r) * factorial(n - r)));
 };
